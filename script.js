402b8f8bd699bd36fcead34763bd2d24cfa8e203
CarlosMercedes <merc0249@student.acitech.org>
2025-03-20 09:14:02 -0400

    Finished UI buttons Created item buttons Created narrator

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 const oppText = document.getElementById("opponentName")
-const resetGameButton = document.getElementById("resetGame")
 
+const roundNum = document.getElementById("roundNum")
+const userNum = document.getElementById("userWins")
+const aiNum = document.getElementById("aiWins")
+
+const resetNumber = document.getElementById("resetCount")
+const resetGameButton = document.getElementById("resetGame")
+const narrator = document.getElementById("roundNarrator")
+
+const rockButton = document.getElementById("itemRock")
+const paperButton = document.getElementById("itemPaper")
+const scissorsButton = document.getElementById("itemScissors")
+const paperclipButton = document.getElementById("itemPaperclip")
+const lampButton = document.getElementById("itemLamp")
+const confirmButton = document.getElementById("confirmButton")
+
+var selectedRock = false
+var selectedPaper = false
+var selectedScissors = false
+var selectedPaperclip = false
+var selectedLamp = false
+
+let timeoutID;
+
+//Opponent gets random name to give vision they are real
 window.onload = function randomizeOppName() {
 
     let randomName = Math.floor(Math.random() * 5) + 1
@@ -29,9 +52,49 @@ window.onload = function randomizeOppName() {
         oppText.textContent = "Alex"
         oppText.style.right = "63.6%"
         }
-
 }
+
+//UI buttons
+resetNumber.addEventListener('click', function () {
+    roundNum.textContent = "Round 0"
+    userNum.textContent = "Your wins: 0"
+    aiNum.textContent = "Opponent wins: 0"
+})
 
 resetGameButton.addEventListener('click', function () {
     location.reload()
 })
+
+//Whatever the player chooses
+rockButton.addEventListener('click', function () {
+    narrator.textContent = "You picked: Rock"
+    narrator.style.fontSize = "42px";
+    confirmButton.style.opacity = "100"
+})
+
+paperButton.addEventListener('click', function () {
+    narrator.textContent = "You picked: Paper"
+    narrator.style.fontSize = "42px";
+    confirmButton.style.opacity = "100"
+})
+
+scissorsButton.addEventListener('click', function () {
+    narrator.textContent = "You picked: Scissors"
+    narrator.style.fontSize = "40px";
+    confirmButton.style.opacity = "100"
+
+})
+
+paperclipButton.addEventListener('click', function () {
+    narrator.textContent = "You picked: Paperclip"
+    narrator.style.fontSize = "38px";
+    confirmButton.style.opacity = "100"
+})
+
+lampButton.addEventListener('click', function () {
+    narrator.textContent = "You picked: Lamp"
+    narrator.style.fontSize = "42px";
+    confirmButton.style.opacity = "100"
+})
+
+//AI is "picking"...
