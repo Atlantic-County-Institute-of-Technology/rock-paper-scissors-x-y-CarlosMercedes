a977bf142bd7de06c0d4bfefe3c8e4b9fc571926
CarlosMercedes <merc0249@student.acitech.org>
2025-03-24 09:13:33 -0400

    Finished making game Need to finish styling and win/round count

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ const paperclipButton = document.getElementById("itemPaperclip")
 const lampButton = document.getElementById("itemLamp")
 const confirmButton = document.getElementById("confirmButton")
 
-const yourArea = document.getElementById("YourArea")
+const yourArea = document.getElementById("yourArea")
 const oppArea = document.getElementById("opponentArea")
 
 //Variables
@@ -80,52 +80,42 @@ resetGameButton.addEventListener('click', function () {
 //Whatever the player chooses
 rockButton.addEventListener('click', function () {
     narrator.textContent = "You picked: Rock";
-    narrator.style.fontSize = "42px";
+    //narrator.style.fontSize = "42px";
     confirmButton.style.display = "block";
-
-        confirmButton.addEventListener('click', function () {
-            playerCount = 1
-        })
-
+    playerCount = 1
+    yourArea.textContent = "rock"
 })
 
 paperButton.addEventListener('click', function () {
     narrator.textContent = "You picked: Paper"
-    narrator.style.fontSize = "42px";
+    //narrator.style.fontSize = "42px";
     confirmButton.style.display = "block";
-
-    confirmButton.addEventListener('click', function () {
-        playerCount = 2
-    })
+    playerCount = 2
+    yourArea.textContent = "paper"
 })
 
 scissorsButton.addEventListener('click', function () {
     narrator.textContent = "You picked: Scissors"
-    narrator.style.fontSize = "40px";
+   // narrator.style.fontSize = "40px";
     confirmButton.style.display = "block";
-
-    confirmButton.addEventListener('click', function () {
-        playerCount = 3
-    })
+    playerCount = 3
+    yourArea.textContent = "scissors"
 })
 
 paperclipButton.addEventListener('click', function () {
     narrator.textContent = "You picked: Paperclip"
-    narrator.style.fontSize = "38px";
+    //narrator.style.fontSize = "38px";
     confirmButton.style.display = "block";
-
-    confirmButton.addEventListener('click', function () {
-        playerCount = 4
-    })
+    playerCount = 4
+    yourArea.textContent = "paperclip"
 })
 
 lampButton.addEventListener('click', function () {
     narrator.textContent = "You picked: Lamp"
-    narrator.style.fontSize = "42px";
+    //narrator.style.fontSize = "42px";
     confirmButton.style.display = "block";
-    confirmButton.addEventListener('click', function () {
-        playerCount = 5
-    })
+    playerCount = 5
+    yourArea.textContent = "lamp"
 })
 
 //Sets the player variable
@@ -142,31 +132,166 @@ confirmButton.addEventListener('click', function () {
     resetGameButton.style.display = "none"
     resetNumber.style.display = "none"
 
+    rockButton.style.display = "none"
+    paperButton.style.display = "none"
+    scissorsButton.style.display = "none"
+    paperclipButton.style.display = "none"
+    lampButton.style.display = "none"
+
     narrator.textContent = "Opponent is picking..."
 
         function aiPickRandom() {
-            let opponentRandomizeCount = Math.floor(Math.random() * 5) + 1
-            oppCount = opponentRandomizeCount
+            let opponentRandomizeCount = Math.round(Math.random() * 5) + 1
+
+            if (opponentRandomizeCount == 1) {
+                oppArea.textContent = "rock"
+            }
+
+            if (opponentRandomizeCount == 2) {
+                oppArea.textContent = "paper"
+            }
+
+            if (opponentRandomizeCount == 3) {
+                oppArea.textContent = "scissors"
+            }
+
+            if (opponentRandomizeCount == 4) {
+                oppArea.textContent = "paperclip"
+            }
+
+            if (opponentRandomizeCount == 5) {
+                oppArea.textContent = "lamp"
+            }
 
             //Decide what the player and AI has 
-            if (playerCount == 1 && oppCount == 1) {
-                narrator.textContent = "You both picked rock! Tie!"
+            if (playerCount == 1 && opponentRandomizeCount == 1) {
+                narrator.textContent = "You both picked rock!"
+                yourArea.style.backgroundColor = "gray"
+                oppArea.style.backgroundColor = "gray"
             }
 
-            if (playerCount == 2 && oppCount == 1) {
-                narrator.textContent = "Rock covers paper! " + randomName +  " wins!"
+            if (playerCount == 2 && opponentRandomizeCount == 1) {
+                narrator.textContent = "Paper covers rock!"
+                yourArea.style.backgroundColor = "green"
             }
 
-            if (playerCount == 3 && oppCount == 1) {
-                narrator.textContent = "Rock breaks scissors! " + randomName +  " wins!"
+            if (playerCount == 3 && opponentRandomizeCount == 1) {
+                narrator.textContent = "Rock breaks scissors!"
+                oppArea.style.backgroundColor = "green"
             }
 
-            if (playerCount == 4 && oppCount == 1) {
-                narrator.textContent = "Rock breaks paperclip! " + randomName +  " wins!"
+            if (playerCount == 4 && opponentRandomizeCount == 1) {
+                narrator.textContent = "Paperclip puts up a fight!"
+                yourArea.style.backgroundColor = "green"
             }
 
-            if (playerCount == 5 && oppCount == 1) {
-                narrator.textContent = "Rock covers paper! " + randomName +  " wins!"
+            if (playerCount == 5 && opponentRandomizeCount == 1) {
+                narrator.textContent = "Rock smashes lamp!"
+                oppArea.style.backgroundColor = "green"
+            }
+
+            if (playerCount == 1 && opponentRandomizeCount == 2) {
+                narrator.textContent = "Paper covers rock!"
+                oppArea.style.backgroundColor = "green"
+            }
+
+            if (playerCount == 2 && opponentRandomizeCount == 2) {
+                narrator.textContent = "You both picked paper!"
+                yourArea.style.backgroundColor = "gray"
+                oppArea.style.backgroundColor = "gray"
+            }
+
+            if (playerCount == 3 && opponentRandomizeCount == 2) {
+                narrator.textContent = "Scissors cut paper!"
+                yourArea.style.backgroundColor = "green"
+            }
+
+            if (playerCount == 4 && opponentRandomizeCount == 2) {
+                narrator.textContent = "Paper engulfes paperclip!"
+                oppArea.style.backgroundColor = "green"
+            }
+
+            if (playerCount == 5 && opponentRandomizeCount == 2) {
+                narrator.textContent = "Lamp burns paper!"
+                yourArea.style.backgroundColor = "green"
+            }
+
+            if (playerCount == 1 && opponentRandomizeCount == 3) {
+                narrator.textContent = "Rock smashes scissors!"
+                yourArea.style.backgroundColor = "green"
+            }
+
+            if (playerCount == 2 && opponentRandomizeCount == 3) {
+                narrator.textContent = "Scissors cut paper!"
+                oppArea.style.backgroundColor = "green"
+            }
+
+            if (playerCount == 3 && opponentRandomizeCount == 3) {
+                narrator.textContent = "You both picked scissors!"
+                yourArea.style.backgroundColor = "gray"
+                oppArea.style.backgroundColor = "gray"
+            }
+
+            if (playerCount == 4 && opponentRandomizeCount == 3) {
+                narrator.textContent = "Paperclip clips scissors closed!"
+                yourArea.style.backgroundColor = "green"
+            }
+
+            if (playerCount == 5 && opponentRandomizeCount == 3) {
+                narrator.textContent = "Scissors cuts lamp cord!"
+                oppArea.style.backgroundColor = "green"
+            }
+
+            if (playerCount == 1 && opponentRandomizeCount == 4) {
+                narrator.textContent = "Paperclip puts up a fight!"
+                oppArea.style.backgroundColor = "green"
+            }
+
+            if (playerCount == 2 && opponentRandomizeCount == 4) {
+                narrator.textContent = "Paper engulfs paperclip!"
+                yourArea.style.backgroundColor = "green"
+            }
+
+            if (playerCount == 3 && opponentRandomizeCount == 4) {
+                narrator.textContent = "Paperclip clips scissors closed!"
+                oppArea.style.backgroundColor = "green"
+            }
+
+            if (playerCount == 4 && opponentRandomizeCount == 4) {
+                narrator.textContent = "You both picked paperclip!"
+                yourArea.style.backgroundColor = "gray"
+                oppArea.style.backgroundColor = "gray"
+            }
+
+            if (playerCount == 5 && opponentRandomizeCount == 4) {
+                narrator.textContent = "Lamp rusts paperclp!"
+                yourArea.style.backgroundColor = "green"
+            }
+
+            if (playerCount == 1 && opponentRandomizeCount == 5) {
+                narrator.textContent = "Rock smashes lamp!"
+                yourArea.style.backgroundColor = "green"
+            }
+
+            if (playerCount == 2 && opponentRandomizeCount == 5) {
+                narrator.textContent = "Lamp burns paper!"
+                oppArea.style.backgroundColor = "green"
+            }
+
+            if (playerCount == 3 && opponentRandomizeCount == 5) {
+                narrator.textContent = "Scissors cuts lamp cord!"
+                yourArea.style.backgroundColor = "green"
+            }
+
+            if (playerCount == 4 && opponentRandomizeCount == 5) {
+                narrator.textContent = "Lamp rusts paperclp!"
+                oppArea.style.backgroundColor = "green"
+            }
+
+            if (playerCount == 5 && opponentRandomizeCount == 5) {
+                narrator.textContent = "You both picked paperclip!"
+                yourArea.style.backgroundColor = "gray"
+                oppArea.style.backgroundColor = "gray"
             }
         }
 
